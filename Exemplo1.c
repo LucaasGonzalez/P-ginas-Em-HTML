2ef333f11dd6d82f92e0ef84d8f1efd2ec9829ee
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 17:04:48 -0300

    Create Exemplo1.c

---
+++ Exemplo1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+ int a[5], b[5], i, somatoria=0;
+
+ for(i=0; i<5; i++)
+ {
+ printf("Qual o valor de a[%d]?\n ", i);
+ scanf("%d", &a[i]);
+ }
+ for(i=0; i<5; i++)
+ {
+ printf("Qual o valor de b[%d]?\n ", i);
+ scanf("%d", &b[i]);
+ }
+
+ for(i=0; i<5; i++)
+ somatoria= somatoria + a[i]+ b[4-i];
+
+
+ printf("\nA somatoria vale:%d ", somatoria);
+
+} 
